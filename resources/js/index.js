de4bdb332b6b5f3788d7ee7cac0e298eb4c11fb2
Fabrizio Garcia <fabgrel10@gmail.com>
2023-01-05 22:41:42 -0600

    🧱 Set variables and initial message

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,1 +1,8 @@
+const inputMessage = document.getElementById('input-message');
+const outputMessage = document.getElementById('output-message');
+const img = outputMessage.querySelector('img');
+let message = document.createTextNode('ingrese el texto aquí');
+const encryptBtn = document.getElementById('encrypt-button');
+const decryptBtn = document.getElementById('decrypt-button');
 
+inputMessage.appendChild(message);
