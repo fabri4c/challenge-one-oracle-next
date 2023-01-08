185832957eafd5b4b8c023b5c03453bb0b2c3d61
Fabrizio Garcia <fabgrel10@gmail.com>
2023-01-07 19:48:46 -0600

    🧱 Implement encrypt and decrypt functions

--- resources/js/index.js
+++ resources/js/index.js
@@ -14,3 +14,36 @@ outputTextarea.value = 'Ingresa el texto que desees encriptar o desencriptar.';
 
 encryptBtn.addEventListener('click', encryptMessage);
 decryptBtn.addEventListener('click', decryptMessage);
+
+function encryptMessage() {
+  const message = inputTextarea.value;
+  const encryptedMessage = CryptoJS.AES.encrypt(message, key).toString();
+
+  removeImgAndPElements();
+
+  outputTextarea.value = encryptedMessage;
+  inputTextarea.value = '';
+}
+
+function decryptMessage() {
+  const encrypted = outputTextarea.value;
+  const decryptedMessage = CryptoJS.AES.decrypt(encrypted, key).toString(
+    CryptoJS.enc.Utf8
+  );
+  inputTextarea.value = decryptedMessage;
+  outputTextarea.value = '';
+}
+
+function removeImgAndPElements() {
+  if (outputContainer.children.length >= 2) {
+    outputContainer.removeChild(img);
+  }
+
+  if (
+    outputTextareaContainer.querySelector('.aside-section__notice') !== null
+  ) {
+    outputTextareaContainer.removeChild(outputTextareaContainer.children[0]);
+  }
+
+  document.getElementById('output-textarea').style.height = '630px';
+}
